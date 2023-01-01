84369d014affe52127e9e032dcb42ef213896fe9
alex-caixiang <121761136+alex-caixiang@users.noreply.github.com>
2023-01-02 00:24:35 +0800

    Add files via upload

---
+++ text.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+
+public class text {
+
+    public static void add(int a,int b){
+        int sum = a + b;
+        System.out.println(sum);
+
+        int d = b - a ;
+        System.out.println(d);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+        int a = 110;
+        int b = 90;
+        add(a,b);
+    }
+}
